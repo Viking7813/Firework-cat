9b74d83a662a2d721575f7870d47178869c8e660
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2024-01-11 20:53:24 +0100

    Börjat med player movement
    Jag får cancer

--- My project/Assets/scripts/Player.cs
+++ My project/Assets/scripts/Player.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    public Transform playerbody;
+
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hi = Input.GetAxis("Horizontal");
+        float vi = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3 (hi * 5f, rb.velocity.y, vi * 5f);
+
+        float mouseX = Input.GetAxis("Mouse X") * 5f;
+        float mouseY = Input.GetAxis("Mouse Y") * 5f;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerbody.Rotate(Vector3.up * mouseX);
     }
 }
