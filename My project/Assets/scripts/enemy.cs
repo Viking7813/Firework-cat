1a47934cbcdc4d0ec5d169797094c167ddec645f
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2024-02-06 14:42:33 +0100

    Enemy funkar typ

--- My project/Assets/scripts/enemy.cs
+++ My project/Assets/scripts/enemy.cs
@@ -5,7 +5,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Enemy: MonoBehaviour
 {
     public NavMeshAgent meshAgent;
 
@@ -13,46 +13,97 @@ public class Enemy : MonoBehaviour
 
     public GameObject player;
 
-    public float velocity, index, distance;
+    public float velocity, index, distance, radius, angle;
 
-    private Vector3 next_pos, destination;
+    public Vector3 next_pos, destination;
 
     public bool chasing = false;
-    private bool R = true;
+    public bool onpath = true;
+    private bool canSeePlayer = false;
+
+    public LayerMask targetMask, obstructionMask;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (R == false)
-        {
-            meshAgent.ResetPath();
-            R = true;
-        }
         if (chasing == false)
         {
+            if (onpath == false)
+            {
+                //(Kanske göra så att den sätte destination till närmaste punkten.)
+
+                meshAgent.SetDestination(destination);
+                if (transform.position.x == destination.x && transform.position.z == destination.z)
+                {
+                    onpath = true;
+                }    
+            }
+            else if (onpath == true)
+            {
+                meshAgent.ResetPath();
                 destination = waypoints[(int)index].transform.position;
                 next_pos = Vector3.MoveTowards(transform.position, destination, velocity * Time.deltaTime);
                 transform.position = next_pos;
 
                 distance = Vector3.Distance(transform.position, destination);
-                if (distance <= 0.05)
+                if (distance <= 0.5)
                 {
                     if (index < waypoints.Count - 1)
                         index++;
+
                     else
                         index = 0;
                 }
+            }
         }
-        if (chasing == true)
+        else if (chasing == true)
         {
             meshAgent.SetDestination(player.transform.position);
-            R = false;
+            onpath = false;
         }
+    }
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+
+        if (canSeePlayer == true)
+            chasing = true;
+        else
+            chasing = false;
     }
 }
